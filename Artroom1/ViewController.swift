413acc45facde3527dcb717fb33ffbbc95c4a98a
Taylor Lindsay <tgracelindsay13@gmail.com>
2018-10-17 14:16:20 +0200

    Initial Commit

---
+++ Artroom1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Artroom1
+//
+//  Created by Taylor Lindsay on 10/17/18.
+//  Copyright © 2018 Taylor Lindsay at Babbel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
