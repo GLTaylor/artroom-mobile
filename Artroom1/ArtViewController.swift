ae4de1b552f21184ab920658ea798a65ce881e62
Taylor Lindsay <tgracelindsay13@gmail.com>
2018-10-30 18:00:27 +0100

    Add button with working shuffle function, not yet DRY but working

--- Artroom1/ArtViewController.swift
+++ Artroom1/ArtViewController.swift
@@ -17,6 +17,7 @@ class ArtViewController: UIViewController {
     @IBOutlet weak var artResults: UILabel!
     @IBOutlet weak var artImage: UIImageView!
 
+    
     var array_of_artworks = [
         (Artwork(title: "Watching - Erin Nicholls", image: "erin nicholls", attributes: ArtAttributes(mood: .happy, interest: .cities ))), (Artwork(title: "Fall Scene - John Smith", image: "nowhere", attributes: ArtAttributes(mood: .happy, interest: .nature))), (Artwork(title: "Mikhail - Annie Leibovitz", image: "Mikhail - Annie Leibovitz", attributes: ArtAttributes(mood: .happy, interest: .humanity))), (Artwork(title: "Reflections - Erin Nicholls", image: "Reflections_nicholls", attributes: ArtAttributes(mood: .sad, interest: .cities))), (Artwork(title: "Norway Night - Sarah Hicks", image: "norway", attributes: ArtAttributes(mood: .sad, interest: .nature))), (Artwork(title: "Diagonal of May 25 - Dan Flavin", image: "Diagonal of May 25 - Dan Flavin", attributes: ArtAttributes(mood: .sad, interest: .humanity))), (Artwork(title: "Melbourne - Erin Nicholls", image: "melbourne", attributes: ArtAttributes(mood: .meh, interest: .cities))), (Artwork(title: "Isle Saint-Michel - Elger Esser", image: "Isle Saint-Michel - Elger Esser", attributes: ArtAttributes(mood: .meh, interest: .nature))), (Artwork(title: "Studies of Hands - Alexander Maw", image: "Studies of Hands - Alexander Maw", attributes: ArtAttributes(mood: .meh, interest: .humanity))), (Artwork(title: "Poveglia - Elger Esser", image: "Poveglia - Elger Esser", attributes: ArtAttributes(mood: .meh, interest: .nature))), (Artwork(title: "Drifting - Anthony Goicolea", image: "Drifting - Anthony Goicolea", attributes: ArtAttributes(mood: .meh, interest: .nature))), (Artwork(title: "My parents with their grandson, Ross - Annie Leibovitz", image: "My parents with their grandson, Ross - Annie Leibovitz", attributes: ArtAttributes(mood: .happy, interest: .humanity))), (Artwork(title: "Class Picture - Anthony Goicolea", image: "Class Picture - Anthony Goicolea", attributes: ArtAttributes(mood: .happy, interest: .humanity)))
     ]
@@ -33,61 +34,27 @@ class ArtViewController: UIViewController {
     
     let rendered = selection.randomElement()
     assignTheLabels(rendered!)
-    
-    // return a random index of selection
-    // set the labels
-    // later for getting just one... func firstIndex(where: (Element) -> Bool) -> Int
-    
-    
-//    switch chosenArtAttributes.mood {
-//    case .happy:
-//        switch chosenArtAttributes.interest {
-//        case .cities:
-//            let artwork = Artwork(title: "Watching - Erin Nicholls", image: "erin nicholls")
-//            assignTheLabels(artwork)
-//
-//        case .nature:
-//            let artwork = Artwork(title: "Fall Scene - John Smith", image: "nowhere")
-//            assignTheLabels(artwork)
-//
-//        case .humanity:
-//            let artwork = Artwork(title: "Mikhail - Annie Leibovitz", image: "Mikhail - Annie Leibovitz")
-//            assignTheLabels(artwork)
-//        }
-//    case .sad:
-//        switch chosenArtAttributes.interest {
-//        case .cities:
-//            let artwork = Artwork(title: "Reflections - Erin Nicholls", image: "Reflections_nicholls")
-//            assignTheLabels(artwork)
-//
-//        case .nature:
-//            let artwork = Artwork(title: "Norway Night - Sarah Hicks", image: "norway")
-//            assignTheLabels(artwork)
-//
-//        case .humanity:
-//            let artything = Artwork(title: "Diagonal of May 25 - Dan Flavin", image: "Diagonal of May 25 - Dan Flavin")
-//            assignTheLabels(artything)
-//        }
-//
-//    case .meh:
-//        switch chosenArtAttributes.interest {
-//        case .cities:
-//            let artwork = Artwork(title: "Melbourne - Erin Nicholls", image: "melbourne")
-//            assignTheLabels(artwork)
-//
-//        case .nature:
-//            let artwork = Artwork(title: "Isle Saint-Michel - Elger Esser", image: "Isle Saint-Michel - Elger Esser")
-//            assignTheLabels(artwork)
-//
-//        case .humanity:
-//            let artwork = Artwork(title: "Studies of Hands - Alexander Maw", image: "Studies of Hands - Alexander Maw")
-//            assignTheLabels(artwork)
-//        }
-//
-//
-//     }
+ 
      }
     
+    
+    // need a button to do all of the above again
+    
+    @IBAction func renderAgain() {
+        func assignTheLabels(_ artwork: Artwork) {
+            self.artResults.text = artwork.title
+            self.artImage.image = UIImage (named: artwork.image)
+        }
+        
+        let selection = array_of_artworks.filter { (Artwork) -> Bool in
+            Artwork.attributes == chosenArtAttributes
+        }
+        
+        let rendered = selection.randomElement()
+        assignTheLabels(rendered!)
+        
+    }
+
     @IBAction func playAgain() {
         self.dismiss(animated: true, completion: nil)
     }
