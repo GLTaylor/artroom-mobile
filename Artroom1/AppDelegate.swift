a1e3dc24e33b23086eeed5da2e3814058a5160d1
Taylor Lindsay <tgracelindsay13@gmail.com>
2018-11-02 18:28:54 +0100

    Create json file deserialized and assigned to array for a functual app

--- Artroom1/AppDelegate.swift
+++ Artroom1/AppDelegate.swift
@@ -15,21 +15,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
       
+        func loadJson(_ fileName: String) -> [Artwork]? {
         if let path = Bundle.main.path(forResource: "data", ofType: "json") {
             do {
                 let data = try Data(contentsOf: URL(fileURLWithPath: path))
-                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
-                if let jsonResult = jsonResult as? Dictionary<String, AnyObject>, let artwork = jsonResult["artwork"] as? [Any] {
-                    print(artwork.count)
-                }
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode(ResponseData.self, from: data)
+                return jsonData.artwork
             } catch {
-                //handle error somehow
+                print("error:\(error)")
             }
+            }
+            return nil
         }
-      
+        arrayOfArtworks = loadJson("data")!
         
         return true
     }
+      
+        
+        
+        
+        
+//
+//
+//        if let path = Bundle.main.path(forResource: "data", ofType: "json") {
+//            do {
+//                let data = try Data(contentsOf: URL(fileURLWithPath: path))
+//                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+//                if let jsonResult = jsonResult as? Dictionary<String, AnyObject>, let artworks = jsonResult["artworks"] as? [Any] {
+//                    print(artworks.count)
+//                }
+//            } catch {
+//                print("error here is:\(error)")
+//            }
+//        }
+
+        
+    }
 
 
-}
+
