99dba10ee81d1310348a16eb3bef79febc412162
Taylor Lindsay <tgracelindsay13@gmail.com>
2018-11-02 17:51:29 +0100

    Include json file and deserialization method (a great victory) - not yet assigned back to an array

--- Artroom1/AppDelegate.swift
+++ Artroom1/AppDelegate.swift
@@ -14,7 +14,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
+      
+        if let path = Bundle.main.path(forResource: "data", ofType: "json") {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path))
+                let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+                if let jsonResult = jsonResult as? Dictionary<String, AnyObject>, let artwork = jsonResult["artwork"] as? [Any] {
+                    print(artwork.count)
+                }
+            } catch {
+                //handle error somehow
+            }
+        }
+      
         
         return true
     }
