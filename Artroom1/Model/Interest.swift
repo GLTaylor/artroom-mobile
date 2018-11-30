a92ff84d9f3101ec71200c47f8a6542318b61f72
Taylor Lindsay <tgracelindsay13@gmail.com>
2018-11-30 17:58:04 +0100

    Fix the mood and interest structs, and account for typos in the api

--- Artroom1/Model/Interest.swift
+++ Artroom1/Model/Interest.swift
@@ -8,8 +8,27 @@
 
 import Foundation
 
-enum Interest: String, Codable {
+enum Interest: Int, Codable {
     case tech
     case nature
     case humanity
+    case sexuality
+    case politics
+    case death
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let string = try container.decode(String.self)
+        
+        switch string {
+        case "Tech ", "Tech": self = .tech
+        case "Nature", "Nature ": self = .nature
+        case "Humanity", "Humanity ": self = .humanity
+        case "Sexuality", "eroticism", "Sexuality ": self = .sexuality
+        case "Politics", "politics", "Politics ": self = .politics
+        case "Death", "death", "Death ": self = .death
+        default: throw
+            DecodingError.dataCorrupted(DecodingError.Context.init(codingPath: container.codingPath, debugDescription: "Unknown string found where a legitimate interest should be \(string) "))
+        }
+    }
 }
--- Artroom1/Model/Mood.swift
+++ Artroom1/Model/Mood.swift
@@ -8,8 +8,29 @@
 
 import Foundation
 
-enum Mood: String, Codable {
-    case joyful
+enum Mood: Int, Codable {
+    case joyful 
     case melancholy
     case meh
+    case wild
+    case poetic
+    case humorous
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let string = try container.decode(String.self)
+        
+        switch string {
+        case "Joyful", "Joyful ": self = .joyful
+        case "Melancholy ", "Melancholy", "melancholy": self = .melancholy
+        case "Meh", "Meh ", "meh": self = .meh
+        case "Wild", "wild", "Wild ": self = .wild
+        case "Poetic", "poetic", "Poetic ": self = .poetic
+        case "Humorous", "Humorous ": self = .humorous
+        default: throw
+            DecodingError.dataCorrupted(DecodingError.Context.init(codingPath: container.codingPath, debugDescription: "Unknown string found where a legitimate mood should be \(string)"))
+        }
+    }
 }
+
+
