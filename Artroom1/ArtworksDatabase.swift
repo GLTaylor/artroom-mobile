5d2ebd205eb677360a77549962a06a1ecbc7257d
Taylor Lindsay <tgracelindsay13@gmail.com>
2018-12-11 08:42:24 +0100

    Undo unhelpful changes

--- Artroom1/ArtworksDatabase.swift
+++ Artroom1/ArtworksDatabase.swift
@@ -9,49 +9,24 @@
 import Foundation
 import UIKit
 
-// this is my handy singleton class
+// singleton class - executes once, but set as shared so I can access what happened here (once) throughout the app.
 
 public class ArtworksDatabase {
     static let shared = ArtworksDatabase()
     private(set) var arrayOfArtworks: [Artwork] // Because I only want to mutate it here but read it from the outside
-    private(set) var dictOfImages: [String: UIImage]
 
     private init() {
         arrayOfArtworks = []
-        dictOfImages = [:]
     }
 
     func load(completionHandler: @escaping () -> Void) {
         loadJson { artworks, _ in
             if let artworks = artworks {
                 self.arrayOfArtworks = artworks
-                // In Progress: The following is supposed to create a dictionary of images, to get them ready for use later instead of making new network requests.
-                artworks.forEach({ artwork in
-                    let imageName = artwork.title
-                    let imageLink = artwork.image.url
-                    let url = URL(string: imageLink)
-                    let task = URLSession.shared.dataTask(with: url!, completionHandler: { data, _, error in
-                        guard let data = data else {
-                            print("location is nil: \(error)")
-                            return
-                        }
-                        guard let image = UIImage(data: data) else {
-                            print("The error is: \(error)")
-                            return
-                        }
-                        DispatchQueue.main.async {
-                            self.dictOfImages.updateValue(image, forKey: imageName)
-                        }
-                    })
-                    task.resume()
-
-                })
                 completionHandler()
             } else {
                 // TODO: handle this error
             }
-            print("the artworks are: \(self.arrayOfArtworks)")
-            print("the images are: \(self.dictOfImages)")
         }
     }
 }
